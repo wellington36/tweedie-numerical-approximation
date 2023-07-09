52eef9f68fb7014b6f06c71ff492d00674daecb0
wellington36 <wellington.71319@gmail.com>
2023-07-09 17:34:46 -0300

    Restructure

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
-import numpy as np
 from scipy import integrate
-from scipy.stats import norm
 import tweedie
-from glum import TweedieDistribution
 
 #print(TweedieDistribution(1.9).log_likelihood(2, 1, 1, 1))
 
@@ -20,7 +17,7 @@ def alpha_to_p(alpha):
 #print(integrate.quad(lambda x: tweedie.tweedie(mu=1, phi=1, p=alpha_to_p(0.5)).pdf(x), 1e-6, 50, points=1000))
 
 
-for i in [0.005, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99, 0.995]:
+for i in [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99]:
     print(i, tweedie.tweedie(mu=1, phi=1.9, p=alpha_to_p(i)).pdf(1))
 
     print(integrate.quad(lambda x: tweedie.tweedie(mu=1, phi=1, p=alpha_to_p(0.5)).pdf(x), 1e-6, 50, points=1000))
--- tweedie/tweedie_naive.py
+++ tweedie/tweedie_naive.py
@@ -1,5 +1,7 @@
 from math import sin, pi, exp
 from scipy.special import gamma
+import numpy as np
+from scipy import integrate
 
 def x(theta, alpha):
     return (alpha - 1) / alpha * (theta / (alpha - 1)) ** alpha
@@ -14,15 +16,20 @@ def N(z, alpha, n):
 
 
 # z, alpha, theta, n
-def pdfz_tweedie_naive(z, theta, alpha, n):
-    assert alpha > 0.01 and alpha < 0.99
+def pdfz_tweedie_naive(z, theta, alpha, n=1000):
+    assert alpha >= 0.01 and alpha <= 0.99
 
     return N(z, alpha, n) * exp(z * theta - x(theta, alpha))
 
-def pdfx_tweedie(x, theta, alpha, lambdaa, n):
+def pdfx_tweedie_naive(x, theta, alpha, lambdaa, n):
     return (1.0/lambdaa) * pdfz_tweedie_naive(x/lambdaa, theta, alpha, n)
 
 
 if __name__ == '__main__':
-    print(pdfz_tweedie_naive(1, -1/2, 1/2, 100))
-    #print(gamma(179))
+    #for alpha in [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99]:
+    #    f = lambda x: pdfz_tweedie_naive(x, -1/2, alpha, n=100)
+
+    #    print(f'alpha: {alpha}, I: {integrate.quad(f, 1e-6, 50, points=1000)}')
+
+    print("This is a module.  Do not run it directly.")
+    exit(1)
