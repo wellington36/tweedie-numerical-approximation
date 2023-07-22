51d7182b831b8fd4ae4c5015d1b9fa2273418fa7
wellington36 <wellington.71319@gmail.com>
2023-07-22 20:00:02 -0300

    Fix error #5

--- test.py
+++ test.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 ############ visualization ############
 def visualization(tweedie_pdf=pdfz_tweedie_dias, alphas=[0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99]):
     for alpha in alphas:
-        f = lambda x: tweedie_pdf(x, -1/2, alpha)    # n not to large
+        f = lambda x: tweedie_pdf(x, -1/2, alpha)
 
-        x = np.linspace(1e-10, 5, 100)
+        x = np.linspace(1e-6, 10, 100)
         y = [f(i) for i in x]
         plt.plot(x, y, label=f'alpha={alpha}')
 
@@ -22,7 +22,9 @@ def test_naive(alphas=[0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99])
     for alpha in alphas:
         f = lambda x: pdfz_tweedie_naive(x, -1/2, alpha, n=80)    # n not to large
 
-        print(f'alpha: {alpha}, I: {integrate.quad(f, 1e-6, 50, points=1000)}')
+        value, _ = integrate.quad(f, 1e-6, 50, points=1000)
+
+        print(f'alpha: {alpha}, I: {(value, abs(1 - value))}')
 
 ############ tweedie dias ############
 def test_dias(alphas=[0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99]):
@@ -30,10 +32,14 @@ def test_dias(alphas=[0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99]):
         f = lambda x: pdfz_tweedie_dias(x, -1/2, alpha)
 
         if alpha == 0.99:
-            print(f'alpha: {alpha}, I: {integrate.quad(f, 1e-6, 20, points=10000)}')
+            value, _ = integrate.quad(f, 1e-6, 20, points=10000)
+
+            print(f'alpha: {alpha}, I: {(value, abs(1 - value))}')
             continue
 
-        print(f'alpha: {alpha}, I: {integrate.quad(f, 1e-6, 50, points=1000)}')
+        value, _ = integrate.quad(f, 1e-6, 50, points=1000)
+
+        print(f'alpha: {alpha}, I: {(value, abs(1 - value))}')
 
 
 ############ main ############
