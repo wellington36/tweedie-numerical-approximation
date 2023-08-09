9aefac54d9d459fca8efc0e36195dd476cab0a8d
wellington36 <wellington.71319@gmail.com>
2023-08-09 16:42:31 -0300

    Initial structure

---
+++ test_tweedie.py
@@ -0,0 +1,44 @@
+from sandbox.tweedie_dias2 import pdfz_tweedie as pdfz_tweedie_dias
+import numpy as np
+from scipy import integrate
+import matplotlib.pyplot as plt
+import time
+
+############ visualization ############
+def visualization(tweedie_pdf=pdfz_tweedie_dias, alphas=[0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99]):
+    for alpha in alphas:
+        f = lambda x: tweedie_pdf(x, -1/2, alpha)
+
+        x = np.linspace(1e-6, 10, 100)
+        y = [f(i) for i in x]
+        plt.plot(x, y, label=f'alpha={alpha}')
+
+    plt.legend()
+    plt.show()
+
+
+############ tweedie dias ############
+def test_dias(alphas=[0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.99]):
+    for alpha in alphas:
+        f = lambda x: pdfz_tweedie_dias(x, -1/2, alpha)
+
+        if alpha == 0.99:
+            value, _ = integrate.quad(f, 1e-6, 20, points=10000)
+
+            print(f'alpha: {alpha}, I: {(value, abs(1 - value))}')
+            continue
+
+        value, _ = integrate.quad(f, 1e-6, 50, points=1000)
+
+        print(f'alpha: {alpha}, I: {(value, abs(value - 1))}')
+
+
+############ main ############
+if __name__ == '__main__':
+
+    t = time.time()
+
+    test_dias(alphas=[0.2])
+    #visualization()
+
+    print(f'time: {time.time() - t}')
